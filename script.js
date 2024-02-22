d5f58b985eca7bc58048f1a95a7a100456f0d5b5
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2024-02-22 19:02:08 -0300

    responsividade

---
+++ script.js
@@ -0,0 +1,10 @@
+let show = 'True';
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () =>{
+
+    menuContent.classList.toggle('on', show);
+    show = !show
+})
